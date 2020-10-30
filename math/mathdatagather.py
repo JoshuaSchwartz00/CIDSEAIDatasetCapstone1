1dae15aa0cdd1ce693c638ab9b82026c8aed23e9
Joshua Schwartz <joshuaschwartz00@gmail.com>
2020-10-30 01:50:26 -0700

    can now add text to an image

--- math/mathdatagather.py
+++ math/mathdatagather.py
@@ -6,6 +6,7 @@ from spacy.symbols import NOUN, NUM
 from spacy.matcher import Matcher
 import re
 from google_images_search import GoogleImagesSearch
+from PIL import Image, ImageDraw, ImageFont
 
 #initializations
 
@@ -37,7 +38,7 @@ data = list(filter(lambda x: len(re.split("(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\
 
 
 
-data = data[:10]
+data = data[:1]
 
 data_formatted = []
 
@@ -51,10 +52,16 @@ for i, data_ob in enumerate(data):
 
     q_doc = nlp(raw_passage)
     nouns = []
+    numbers = []
     for token in q_doc :
         if token.pos == NOUN and pattern.match(token.text) != None:
             nouns.append(token.text)
+        elif token.pos == NUM:
+            numbers.append(token.text)
     data_ob["nouns"] = nouns
+
+    print(nouns)
+    print(numbers)
 
     for n, noun in enumerate(nouns):
 
@@ -62,15 +69,31 @@ for i, data_ob in enumerate(data):
         data_ob["question"] = data_ob["question"].replace(noun, '[{:02d}]'.format(n), 1)
 
         #searches google and saves enumerated images to an enumerated folder
-        # search_params = {
-        #     'q': noun,
-        #     'num': 1,
-        #     'safe': 'high',
-        #     'fileType': 'png',
-        # }
-        # gis.search(search_params=search_params, 
-        #     path_to_dir= "./img/{:04d}/".format(i), 
-        #     custom_image_name = '{:02d}'.format(n))
+        search_params = {
+            'q': noun,
+            'num': 1,
+            'safe': 'high',
+            'fileType': 'png',
+        }
+        gis.search(search_params=search_params, 
+            path_to_dir= "./img/{:04d}/".format(i), 
+            custom_image_name = '{:02d}'.format(n))
+
+        # picture = "./img/{:04d}/".format(i) + '{:02d}'.format(n) + ".png"
+
+        # #open image as a background
+        # with Image.open('{:02d}'.format(n) + ".png") as image:
+        #     draw = ImageDraw.Draw(image)
+
+        #     #set up message
+        #     font = ImageFont.truetype("Roboto-Bold.ttf", size = 30)
+        #     (x, y) = (50, 50)
+        #     message = "some number"
+        #     color = "rgb(0, 0, 0)"
+
+        #     draw.text((x, y), message, fill=color, font=font)
+            
+        #     image.save(picture)
 
     new_data = {}
 
@@ -85,8 +108,27 @@ for i, data_ob in enumerate(data):
     new_data["A"] = data_ob["options"]
     new_data["I"] = "{:04d}".format(i)
 
+    for n, noun in enumerate(nouns):
+        data_formatted.append(new_data)
+
+        picture = "./img/{:04d}/".format(i) + '{:02d}'.format(n) + ".jpg"
+
+        with Image.open(picture) as image:
+                    draw = ImageDraw.Draw(image)
+
+                    #set up message
+                    font = ImageFont.truetype("arial.ttf", size = 30)
+                    (x, y) = (50, 50)
+                    message = "some number"
+                    color = "rgb(0, 0, 0)"
+
+                    draw.text((x, y), message, fill=color, font=font)
+                    
+                    image.save(picture)
 
     data_formatted.append(new_data)
+
+
 with open('train.save.json', 'w') as f:
     f.write(json.dumps(data_formatted, indent=4))
     f.close()
